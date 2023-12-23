fdf6a45592afca7eacc012ca5b4a5248f061077e
XuanThao <vxuanthao03@gmail.com>
2023-12-23 23:58:41 +0700

    nay resolve conflict lỡ tay xóa 1 dòng nen fix ne  :DD

--- hotiul/src/assets/icons/index.js
+++ hotiul/src/assets/icons/index.js
@@ -1,4 +1,3 @@
-
 export { default as IC_booking } from "./IC_booking.svg";
 export { default as IC_customer } from "./IC_customer.svg";
 export { default as IC_dashboard } from "./IC_dashboard.svg";
@@ -14,11 +13,9 @@ export { default as IC_navDetail } from "./IC_navDetail.svg";
 export { default as IC_sort } from "./IC_sort.svg";
 export { default as IC_nextArrow } from "./IC_nextArrow.svg";
 export { default as IC_backArrow } from "./IC_backArrow.svg";
-
-export { default as IC_closebutton } from './IC_closebutton.svg';
+export { default as IC_line } from "./IC_line.svg";
+export { default as IC_closebutton } from "./IC_closebutton.svg";
 export { default as IC_CircleCheck } from "./IC_CircleCheck.svg";
 export { default as IC_CircleLogout } from "./IC_CircleLogout.svg";
 export { default as IC_CirclePerson } from "./IC_CirclePerson.svg";
 export { default as IC_CircleDollar } from "./IC_CircleDollar.svg";
-
-

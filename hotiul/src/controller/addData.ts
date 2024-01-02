69874f92c5d2b6a5b00044999d0c7683df3b0c87
TranChinh03 <21520166@gm.uit.edu.vn>
2024-01-03 04:32:10 +0700

    testing loading animation

---
+++ hotiul/src/controller/addData.ts
@@ -0,0 +1,42 @@
+import { setDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
+type params = {
+  id: string;
+  table:
+    | "Voucher"
+    | "Partner"
+    | "Group_Product"
+    | "Shelf"
+    | "Product"
+    | "Orders"
+    | "Ware_House";
+
+  data:
+    | TVoucher
+    | TPartner
+    | TGroupProduct
+    | TProduct
+    | TOrder
+    | TShelf
+    | TWarehouse;
+};
+const addData = async ({ data, table, id }: params) => {
+  let userId = window.localStorage.getItem("USER_ID");
+  await setDoc(doc(db, `/Manager/${userId}/${table}`, id), data)
+    .then(() => {
+      console.log(">>>>>>>>>> Add Data >>>>>>>>>>");
+    })
+    .catch((e) => console.log(e));
+};
+
+const updateData = async ({ data, table, id }: params) => {
+  let userId = window.localStorage.getItem("USER_ID");
+  //console.log("OKKK");
+  await updateDoc(doc(db, `/Manager/${userId}/${table}`, id), data)
+    .then(() => {
+      console.log(">>>>>>>>>> Update Data >>>>>>>>>>");
+    })
+    .catch((e) => console.log(e));
+};
+export { addData, updateData };
---
+++ hotiul/src/controller/deleteData.ts
@@ -0,0 +1,18 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+type params = {
+  id: string;
+  table:
+    | "Voucher"
+    | "Partner"
+    | "Group_Product"
+    | "Product"
+    | "Orders"
+    | "Ware_House"
+    | "Shelf";
+};
+const deleteData = async ({ id, table }: params) => {
+  let userId = window.localStorage.getItem("USER_ID");
+  await deleteDoc(doc(db, `/Manager/${userId}/${table}`, id));
+};
+export { deleteData };

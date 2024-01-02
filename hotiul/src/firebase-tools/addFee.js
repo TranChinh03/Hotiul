4eafe672d6e2937a1653f7cb6ac467b50b693a91
ziuwuchuoi <vv0320nt@gmail.com>
2024-01-03 03:53:04 +0700

    check giup em zi data

---
+++ hotiul/src/firebase-tools/addFee.js
@@ -0,0 +1,72 @@
+const FEE = [
+	{
+		ID: 'F001',
+		Name: 'Month 1 Fee',
+		Details: [
+			{
+				Name: 'Water',
+				Price: 100000,
+			},
+			{
+				Name: 'Electricity',
+				Price: 200000,
+			},
+		],
+	},
+	{
+		ID: 'F002',
+		Name: 'Month 2 Fee',
+		Details: [
+			{
+				Name: 'Water',
+				Price: 185000,
+			},
+			{
+				Name: 'Electricity',
+				Price: 245000,
+			},
+		],
+	},
+	{
+		ID: 'F003',
+		Name: 'Month 3 Fee',
+		Details: [
+			{
+				Name: 'Water',
+				Price: 115000,
+			},
+			{
+				Name: 'Electricity',
+				Price: 210000,
+			},
+		],
+	},
+	{
+		ID: 'F004',
+		Name: 'Month 4 Fee',
+		Details: [
+			{
+				Name: 'Water',
+				Price: 135000,
+			},
+			{
+				Name: 'Electricity',
+				Price: 220000,
+			},
+		],
+	},
+	{
+		ID: 'F005',
+		Name: 'Month 5 Fee',
+		Details: [
+			{
+				Name: 'Water',
+				Price: 250000,
+			},
+			{
+				Name: 'Electricity',
+				Price: 180000,
+			},
+		],
+	},
+];
---
+++ hotiul/src/firebase-tools/addRoomType.js
@@ -0,0 +1,281 @@
+const ROOM_TYPE = [
+	{
+		TypeID: 'RTSingleReg',
+		TypeName: 'Single Regular Room',
+		NumPerson: 1,
+		Amenities: [
+			{
+				Name: 'Dental Kit',
+				Quantity: 1,
+			},
+			{
+				Name: 'Comb',
+				Quantity: 1,
+			},
+			{
+				Name: 'Shampoo',
+				Quantity: 1,
+			},
+			{
+				Name: 'Bath Gel',
+				Quantity: 1,
+			},
+			{
+				Name: 'Sanitary Bag',
+				Quantity: 2,
+			},
+			{
+				Name: 'Slipper',
+				Quantity: 1,
+			},
+			{
+				Name: 'Towel',
+				Quantity: 2,
+			},
+			{
+				Name: 'Bed',
+				Quantity: 1,
+			},
+			{
+				Name: 'Television',
+				Quantity: 1,
+			},
+			{
+				Name: 'Water Heater',
+				Quantity: 1,
+			},
+			{
+				Name: 'Air Conditioner',
+				Quantity: 1,
+			},
+		],
+	},
+	{
+		TypeID: 'RTCoupleReg',
+		TypeName: 'Couple Regular Room',
+		NumPerson: 2,
+		Amenities: [
+			{
+				Name: 'Dental Kit',
+				Quantity: 2,
+			},
+			{
+				Name: 'Comb',
+				Quantity: 2,
+			},
+			{
+				Name: 'Shampoo',
+				Quantity: 2,
+			},
+			{
+				Name: 'Bath Gel',
+				Quantity: 2,
+			},
+			{
+				Name: 'Sanitary Bag',
+				Quantity: 4,
+			},
+			{
+				Name: 'Slipper',
+				Quantity: 2,
+			},
+			{
+				Name: 'Towel',
+				Quantity: 4,
+			},
+			{
+				Name: 'Bed',
+				Quantity: 1,
+			},
+			{
+				Name: 'Television',
+				Quantity: 1,
+			},
+			{
+				Name: 'Water Heater',
+				Quantity: 1,
+			},
+			{
+				Name: 'Air Conditioner',
+				Quantity: 1,
+			},
+		],
+	},
+	{
+		TypeID: 'RTSingleVIP',
+		TypeName: 'Single VIP Room',
+		NumPerson: 1,
+		Amenities: [
+			{
+				Name: 'Dental Kit',
+				Quantity: 1,
+			},
+			{
+				Name: 'Comb',
+				Quantity: 1,
+			},
+			{
+				Name: 'Shampoo',
+				Quantity: 1,
+			},
+			{
+				Name: 'Bath Gel',
+				Quantity: 1,
+			},
+			{
+				Name: 'Sanitary Bag',
+				Quantity: 2,
+			},
+			{
+				Name: 'Slipper',
+				Quantity: 1,
+			},
+			{
+				Name: 'Towel',
+				Quantity: 2,
+			},
+			{
+				Name: 'Bed',
+				Quantity: 1,
+			},
+			{
+				Name: 'Television',
+				Quantity: 1,
+			},
+			{
+				Name: 'Water Heater',
+				Quantity: 1,
+			},
+			{
+				Name: 'Air Conditioner',
+				Quantity: 1,
+			},
+			{
+				Name: 'Hair Dryer',
+				Quantity: 1,
+			},
+			{
+				Name: 'Refrigerator',
+				Quantity: 1,
+			},
+		],
+	},
+	{
+		TypeID: 'RTCoupleVIP',
+		TypeName: 'Couple VIP Room',
+		NumPerson: 2,
+		Amenities: [
+			{
+				Name: 'Dental Kit',
+				Quantity: 2,
+			},
+			{
+				Name: 'Comb',
+				Quantity: 2,
+			},
+			{
+				Name: 'Shampoo',
+				Quantity: 2,
+			},
+			{
+				Name: 'Bath Gel',
+				Quantity: 2,
+			},
+			{
+				Name: 'Sanitary Bag',
+				Quantity: 4,
+			},
+			{
+				Name: 'Slipper',
+				Quantity: 2,
+			},
+			{
+				Name: 'Towel',
+				Quantity: 4,
+			},
+			{
+				Name: 'Bed',
+				Quantity: 1,
+			},
+			{
+				Name: 'Television',
+				Quantity: 1,
+			},
+			{
+				Name: 'Water Heater',
+				Quantity: 1,
+			},
+			{
+				Name: 'Air Conditioner',
+				Quantity: 1,
+			},
+			{
+				Name: 'Hair Dryer',
+				Quantity: 1,
+			},
+			{
+				Name: 'Refrigerator',
+				Quantity: 1,
+			},
+		],
+	},
+	{
+		TypeID: 'RTSmallF',
+		TypeName: 'Small Family Room',
+		NumPerson: 4,
+		Amenities: [
+			{
+				Name: 'Dental Kit',
+				Quantity: 4,
+			},
+			{
+				Name: 'Comb',
+				Quantity: 4,
+			},
+			{
+				Name: 'Shampoo',
+				Quantity: 4,
+			},
+			{
+				Name: 'Bath Gel',
+				Quantity: 4,
+			},
+			{
+				Name: 'Sanitary Bag',
+				Quantity: 8,
+			},
+			{
+				Name: 'Slipper',
+				Quantity: 4,
+			},
+			{
+				Name: 'Towel',
+				Quantity: 8,
+			},
+			{
+				Name: 'Bed',
+				Quantity: 2,
+			},
+			{
+				Name: 'Television',
+				Quantity: 1,
+			},
+			{
+				Name: 'Water Heater',
+				Quantity: 1,
+			},
+			{
+				Name: 'Air Conditioner',
+				Quantity: 1,
+			},
+			{
+				Name: 'Hair Dryer',
+				Quantity: 1,
+			},
+			{
+				Name: 'Refrigerator',
+				Quantity: 1,
+			},
+		],
+	},
+];

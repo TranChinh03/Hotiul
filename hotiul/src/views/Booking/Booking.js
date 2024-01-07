6fc142f140b51738ea1c00135c6e483c4d838a05
TranChinh03 <21520166@gm.uit.edu.vn>
2024-01-07 19:46:35 +0700

    fix UI

--- hotiul/src/views/Booking/Booking.js
+++ hotiul/src/views/Booking/Booking.js
@@ -144,8 +144,9 @@ export const Booking = () => {
                     const tData = val[accessor] ? val[accessor] : "——";
                     return <td className={styles.col}>{tData}</td>;
                   })}
-                  <td className={styles.colDetail}>
-                    View Full Detail <img className="pl-2" src={IC_navDetail} />
+                  <td className={styles.col}>
+                  <p>View Full Detail </p>
+                    <img className="pl-2" src={IC_navDetail} />
                   </td>
                 </tr>
               );

8da41f313402b0034e2252d1dc5db18df334cd18
ziuwuchuoi <vv0320nt@gmail.com>
2024-01-09 23:19:44 +0700

    trans

--- hotiul/src/views/Staff/component/addStaff.js
+++ hotiul/src/views/Staff/component/addStaff.js
@@ -5,31 +5,22 @@ import { Button, Select, message } from 'antd';
 import { addData, updateData } from '../../../controller/addData.ts';
 import { createID } from '../../../utils/appUtils.js';
 import { IM_Camera } from '../../../assets/imgs/index.js';
-import { useTranslation } from "react-i18next"
-import {
-	deleteObject,
-	getDownloadURL,
-	ref,
-	uploadBytes,
-  } from "firebase/storage";
-import { storage } from "../../../firebaseConfig.js";
-
+import { useTranslation } from 'react-i18next';
+import { deleteObject, getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { storage } from '../../../firebaseConfig.js';
 
 function AddStaff(props) {
-
-
 	const { t } = useTranslation();
 	const [selectedImage, setSelectedImage] = useState(null);
 	const [selectedImageFile, setSelectedImageFile] = useState(null);
 	const fileInputRef = useRef(null);
 
-
-	const handleImageSelected = (event) => {
+	const handleImageSelected = event => {
 		if (event.target.files && event.target.files[0]) {
-		  setSelectedImageFile(event.target.files[0]);
-		  setSelectedImage(URL.createObjectURL(event.target.files[0]));
+			setSelectedImageFile(event.target.files[0]);
+			setSelectedImage(URL.createObjectURL(event.target.files[0]));
 		}
-	  };	  
+	};
 
 	const [edit, setEdit] = useState({
 		name: '',
@@ -51,29 +42,25 @@ function AddStaff(props) {
 		});
 	}
 
-
 	async function handleSave() {
-		if (edit.name === "" || edit.ctzId === "" || edit.phone === "" || edit.gender === ""){
-			message.error("Please fill full information!")
-			return
+		if (edit.name === '' || edit.ctzId === '' || edit.phone === '' || edit.gender === '') {
+			message.error('Please fill full information!');
+			return;
 		}
-		const staffID = createID({ prefix: "S" })
-		if (props.fullData.findIndex(x => x.CitizenID === edit.ctzId || x.Phone === edit.phone) !== -1)
-		{
-			message.error("User Existed!")
-			return
+		const staffID = createID({ prefix: 'S' });
+		if (
+			props.fullData.findIndex(x => x.CitizenID === edit.ctzId || x.Phone === edit.phone) !== -1
+		) {
+			message.error('User Existed!');
+			return;
 		}
 
 		try {
-			
-			var imageLink = "";
-			const storageRef = ref(
-				storage,
-				`Images/Staff/${staffID}`
-			  );
-			  if (selectedImageFile !== null) {
+			var imageLink = '';
+			const storageRef = ref(storage, `Images/Staff/${staffID}`);
+			if (selectedImageFile !== null) {
 				const snapshot = await uploadBytes(storageRef, selectedImageFile);
-				imageLink = await getDownloadURL(snapshot.ref)
+				imageLink = await getDownloadURL(snapshot.ref);
 			}
 
 			const newData = {
@@ -87,12 +74,12 @@ function AddStaff(props) {
 				Username: edit.username,
 				Password: edit.password,
 
-				Role: "Staff",
+				Role: 'Staff',
 				Image: imageLink,
-			}
-			console.log(newData)
-			addData({ data: newData, table: "STAFF", id: staffID });
-			message.success("Add new user successfully!")
+			};
+			console.log(newData);
+			addData({ data: newData, table: 'STAFF', id: staffID });
+			message.success('Add new user successfully!');
 
 			setEdit({
 				name: '',
@@ -105,9 +92,9 @@ function AddStaff(props) {
 				password: '',
 
 				image: '',
-			})
-			setSelectedImage(null)
-			setSelectedImageFile(null)
+			});
+			setSelectedImage(null);
+			setSelectedImageFile(null);
 
 			props.setOpen(false);
 			props.fetchData();
@@ -129,17 +116,16 @@ function AddStaff(props) {
 			username: '',
 			password: '',
 
-			image: ''
-		})
-		setSelectedImage(null)
-		setSelectedImageFile(null)
-
+			image: '',
+		});
+		setSelectedImage(null);
+		setSelectedImageFile(null);
 	}
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.header}>
-					<div className={styles.headerTitle}>Add new staff</div>
+					<div className={styles.headerTitle}>Add New staff</div>
 					<>
 						<div className={styles.buttonContainer}>
 							<button
@@ -168,42 +154,42 @@ function AddStaff(props) {
 				<div className={styles.infoContainer}>
 					<div className={styles.avatarContainer}>
 						<div className="px-2 mt-2 col-span-3 inline-block">
-						<div
-							onClick={() => fileInputRef.current.click()}
-							style={{
-							padding: "5px",
-							border: "1px solid gray",
-							borderRadius: "10px",
-							width: "fit-content",
-							}}
-							className="cursor-pointer m-auto"
-						>
-							{selectedImage ? (
-								<img
-								src={selectedImage}
-								alt="Selected"
-								style={{ width: "100%", maxHeight: "200px" }}
+							<div
+								onClick={() => fileInputRef.current.click()}
+								style={{
+									padding: '5px',
+									border: '1px solid gray',
+									borderRadius: '10px',
+									width: 'fit-content',
+								}}
+								className="cursor-pointer m-auto">
+								{selectedImage ? (
+									<img
+										src={selectedImage}
+										alt="Selected"
+										style={{ width: '100%', maxHeight: '200px' }}
+									/>
+								) : (
+									<img src={IM_Camera} />
+								)}
+								<input
+									type="file"
+									accept="image/*"
+									ref={fileInputRef}
+									style={{ display: 'none' }}
+									onChange={handleImageSelected}
 								/>
-							) : (
-							<img src={IM_Camera}/>
-							)}
-							<input
-							type="file"
-							accept="image/*"
-							ref={fileInputRef}
-							style={{ display: "none" }}
-							onChange={handleImageSelected}
-							/>
-						</div>
-						{selectedImage ? (
-							<Button
-							onClick={() => {
-								setSelectedImage(null)
-								setSelectedImageFile(null)
-							}}
-							style={{ margin: "10px auto" }}
-						>{t("button.deleteImg")}</Button>) : null
-						}  
+							</div>
+							{selectedImage ? (
+								<Button
+									onClick={() => {
+										setSelectedImage(null);
+										setSelectedImageFile(null);
+									}}
+									style={{ margin: '10px auto' }}>
+									{t('button.deleteImg')}
+								</Button>
+							) : null}
 						</div>
 					</div>
 					<div className={styles.infoDetail}>
